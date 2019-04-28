447085d6c9c0f1893ffe6cdc2d3f29435faf2aa9
zhoudw <396996717@qq.com>
2019-04-28 17:17:50 +0800

    二叉排序树

---
+++ datastructure/src/demo11/Node.java
@@ -0,0 +1,99 @@
+package demo11;
+
+/**
+ * Created by zdw
+ * 2019-04-28 11:43.
+ */
+public class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    /**
+     * 向子树中添加节点
+     * @param node
+     */
+    public void add(Node node) {
+        if(node==null){
+            return;
+        }
+        //判断传入的节点的值比当前子树的根节点的值大还是小
+        //添加的节点比当前节点的值更小
+        if(node.value<this.value){
+            //如果左节点为空
+            if (this.left==null) {
+                this.left=node;
+             //如果不为空
+            }else {
+                this.left.add(node);
+            }
+        }else {
+            if (this.right==null) {
+                this.right=node;
+            }else{
+                this.right.add(node);
+            }
+        }
+    }
+
+    //中序遍历
+    public void midShow(Node node) {
+        if (node==null) {
+            return;
+        }
+        if (node.left!=null) {
+            midShow(node.left);
+        }
+        System.out.println(node.value);
+        if (node.right!=null) {
+            midShow(node.right);
+        }
+    }
+
+    /**
+     * 查找节点
+     * @param value
+     * @return
+     */
+    public Node search(int value){
+        if (this.value==value) {
+            return this;
+        }else if(value<this.value){
+            if (left==null) {
+                return null;
+            }
+            return left.search(value);
+        }else if (value>this.value){
+            if (right==null) {
+                return null;
+            }
+            return right.search(value);
+        }
+        return null;
+    }
+
+    /**
+     * 搜索父节点
+     * @param value
+     * @return
+     */
+    public Node searchParent(int value) {
+        if ((this.left!=null && this.left.value==value)||(this.right!=null && this.right.value==value)) {
+            return this;
+        }else {
+            if(this.value>value && this.left!=null) {
+                return this.left.searchParent(value);
+            }else if(this.value<value && this.right!=null){
+                return this.right.searchParent(value);
+            }
+        }
+        return null;
+    }
+
+
+
+}
---
+++ datastructure/src/demo11/TestBinarySortTree.java
@@ -0,0 +1,31 @@
+package demo11;
+
+/**
+ * Created by zdw
+ * 2019-04-28 14:05.
+ */
+public class TestBinarySortTree {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {7,3,10,12,5,1,9};
+        //创建一颗二叉排序树
+        BinarySortTree bst = new BinarySortTree();
+
+        for (int i = 0; i < arr.length; i++) {
+            bst.add(new Node(arr[i]));
+        }
+        //
+        bst.midShow();
+//        Node node = bst.search(20);
+//        System.out.println(node);
+        //测试查找父节点
+//        Node p1 =  bst.searchParent(3);
+//        System.out.println(p1.value);
+//        bst.delete(9);
+        bst.delete(7);
+        bst.midShow();
+
+    }
+
+
+}

8e7e49feb80753f4e24dd36ae5630a0d2053ac40
zhoudw <zdw941008@sina.com>
2019-06-12 13:04:59 +0800

    test2

--- datastructure/src/hellow/Test.java
+++ datastructure/src/hellow/Test.java
@@ -9,6 +9,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("test1");
+        System.out.println("test2");
     }
 
 }
